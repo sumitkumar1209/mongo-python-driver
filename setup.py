3e544863baf0a2b850220e086b2299ff8c22b03c
Sumit Kumar <sumitkumar1209@gmail.com>
2016-08-01 19:19:59 +0530

    name change for internal compatibility

--- setup.py
+++ setup.py
@@ -279,7 +279,7 @@ else:
     extra_opts['ext_modules'] = ext_modules
 
 setup(
-    name="pymongo",
+    name="pymongo3",
     version=version,
     description="Python driver for MongoDB <http://www.mongodb.org>",
     long_description=readme_content,
